852d1012766b7a64677561c126ae8715044e29b8
augustusyu <ygh690181479@gmail.com>
2019-12-06 16:07:12 -0600

    Mac push

--- predictfuns.py
+++ predictfuns.py
@@ -3,6 +3,16 @@
 import numpy as np
 from sklearn.metrics.pairwise import pairwise_distances, cosine_similarity
 from sklearn.metrics import mean_squared_error
+import scipy.sparse as sp
+from scipy.sparse.linalg import svds
+import surprise
+from surprise.prediction_algorithms.matrix_factorization import NMF as nmf
+import lightfm
+from lightfm.evaluation import precision_at_k
+from lightfm.evaluation import recall_at_k
+from lightfm.datasets import fetch_movielens
+from scipy.sparse import coo_matrix
+
 
 def predict(u,i):
     return u*i
@@ -26,7 +36,8 @@ def usercf(trainmx, testmx):
     pred = pred / np.array([np.abs(sim).sum(axis=1)]).T
     pred += mean[:,np.newaxis]
     pred[np.isnan(pred)] = 0
-    return rmse(pred, testmx)
+    #return rmse(pred, testmx)
+    return pred
 
 def itemcf(trainmx, testmx):
     print('begin item cf')
@@ -34,12 +45,107 @@ def itemcf(trainmx, testmx):
     pred = np.dot(trainmx, sim)
     pred = pred / np.array([np.abs(sim).sum(axis=1)])
     pred[np.isnan(pred)] = 0
-    return rmse(pred, testmx)
+    #return rmse(pred, testmx)
+    return pred
 
+
+def mysvd(trainmx,f):
+    print('begin svd algo')
+    u, s, vt = svds(trainmx, k=f)
+    s_diag_matrix = np.diag(s)
+    X_pred = np.dot(np.dot(u, s_diag_matrix), vt)
+    return X_pred
+
+
+def mynmf(trainmx,testmx, f):
+    print ('begin nmf algo')
+    A = nmf(n_factors = f)
+    A.fit(trainmx)
+    return A.test(testmx)
+
+
+def mylightfm(trainmx, testmx, f ):
+    print('begin light mf model')
+    model = lightfm.LightFM(no_components = f,loss='warp')
+    coo_trainmx = coo_matrix(trainmx)
+    model.fit(coo_trainmx)
+    pred = np.zeros(trainmx.shape)
+    item = np.arange(0,trainmx.shape[1])
+    for i in range (trainmx.shape[0]):
+        #for j in range(trainmx.shape[1]):
+        pred[i,:] = model.predict(i,item)
+    print('finish light fm ')
+    return pred
+
+
+def trainmodel(trainmx, testmx, f,mode):
+    if mode == 0:
+        return usercf(trainmx, testmx)
+    elif mode == 1 :
+        return itemcf(trainmx, testmx)
+    elif mode == 2 :
+        return mysvd(trainmx, f)
+    elif mode == 3 :
+        return mynmf(trainmx, testmx,f)
+    elif mode == 4:
+        return mylightfm(trainmx, testmx, f)
+    else:
+        print('check for model parameter')
 
 def rmse(pred, test):
     p = pred[test.nonzero()].flatten()
     t = test[test.nonzero()].flatten()
     return mean_squared_error (p,t)
 
+def calhr(pred,testmx,t):
+    hr = np.zeros(pred.shape[0],dtype = np.float)
+    n = 0
+    for i in range(pred.shape[0]):
+        if np.sum(testmx[i,:]) == 0:
+            hr[i] = 0
+        else:
+            n += 1
+            a = np.argsort(-pred[0,:])
+            for k in range(pred.shape[1]):
+                if testmx[i,k] != 0 and k in a[:t]:
+                    hr[i] += 1.0
+            hr[i] = hr[i] / np.sum(testmx[i,:]!=0)
+    return np.sum(hr) / n
 
+def calhrlist(pred,testmx,tlist):
+    hr = np.zeros([pred.shape[0], np.size(tlist)],dtype = np.float)
+    n = 0
+    for i in range(pred.shape[0]):
+        if np.sum(testmx[i,:]) == 0:
+            hr[i] = 0
+        else:
+            n += 1
+            a = np.argsort(-pred[0,:])
+            for k in range(pred.shape[1]):
+                if testmx[i,k] != 0 :
+                    for f in range(len(tlist)):
+                        if k in a[:tlist[f]]:
+                            hr[i,f:] += 1.0
+                            break
+            hr[i,:] = hr[i,:] / np.sum(testmx[i,:]!=0)
+    return np.sum(hr,axis = 0) / n
+
+def calhit(pred,testmx, t,hitlist):
+    y = hitlist.copy()
+    for i in range(len(hitlist)):
+        if hitlist[i] == 0 and np.sum(testmx[i,:]) != 0:
+            a = np.argsort(-pred[0,:])
+            for k in range(pred.shape[1]):
+                if testmx[i,k] != 0 :
+                    if k in a[:t]:
+                        y[i] = 1
+                        break
+
+    return y
+
+def gethitmx(trainmx, alltrainmx, hitlist):
+    y = trainmx.copy()
+    for i in range(len(hitlist)):
+        if hitlist[i] !=0:
+            y[i,:] = alltrainmx[i,:]
+    return y
--- recomtest.py
+++ recomtest.py
@@ -21,10 +21,16 @@ def trainsplit(p, outp):
     test = data[idx[trainnum:],0:3]
     print(train.shape)
     trainmerge = np.zeros([10, train.shape[0], train.shape[1]],dtype = np.int32)
+    # for i in range(10):
+    #     p = i/10.0+0.1
+    #     trainmerge[i,:,:] = trainmergef(train, p)
+    idx = np.arange(train.shape[0])
+    np.random.shuffle(idx)
     for i in range(10):
-        p = i/10.0+0.1
-        trainmerge[i,:,:] = trainmergef(train, p)
+        trainmerge[i,:,:] = train.copy()
+        trainmerge[i,idx[:np.int(train.shape[0]* (i/10.0+0.1))],2] = 4
     np.save(outp, [train, test, trainmerge, unum, pnum])
+
 
 def trainmergef(data, p ):
     # use 4 to merge
@@ -34,8 +40,10 @@ def trainmergef(data, p ):
     np.random.shuffle(idx)
     l = np.int(p*num)
     for i in range(l):
-        y[idx[i],2] = 3
+        y[idx[i],2] = 4
     return y.astype(np.int32)
+
+
 
 
 def mainmodel(p):
@@ -43,25 +51,145 @@ def mainmodel(p):
     train = data[0]
     test = data[1]
     trainmerge = data[2]
-    # print(train.shape)
-    # print(trainmerge.shape)
     unum = data[3]
     pnum = data[4]
-
     testmx = data2mx(test,unum,pnum)
     trainmx = data2mx(train,unum,pnum)
     tstart = time.time()
+    print('train mx is ',np.sum(trainmx))
+    print('test mx is ',np.sum(testmx))
+    algo = 2
+    f = 10
+    hrlist = np.array([10,30,100,300])
+    hr = np.zeros([11, np.size(hrlist)])
+    pred = trainmodel(trainmx, testmx, f, algo)
+    print('user is ',pred.shape[0])
+    print('item is ',pred.shape[1])
 
-    outd = np.zeros(11)
-    outd[0] = itemcf(trainmx, testmx)
-    print('rmse = %f in %f seconds in i = 0'%(outd[0], time.time()-tstart))
+    hr[0,:] = calhrlist(pred,testmx,hrlist)
+    print('%f seconds in i = 0' % (time.time() - tstart), hr[0,:])
     for i in range(10):
         a = trainmerge[i,:,:].reshape([train.shape[0], train.shape[1]])
         trainmx = data2mx(a, unum, pnum)
-        outd[i+1] = itemcf(trainmx, testmx)
-        print('rmse = %f in %f seconds in i = %d' % (outd[i+1], time.time() - tstart, i+1))
-    outp = 'itemcf-3.mat'
-    scio.savemat(outp, {'usercf':outd})
+        pred = trainmodel(trainmx, testmx, f, algo)
+        hr[i+1, :] = calhrlist(pred, testmx, hrlist)
+        print('%f seconds in i = %d' % (time.time() - tstart,i+1), hr[i+1, :])
+    outp = 'test1130-1.mat'
+    scio.savemat(outp, {'hr':hr,'hrlist':hrlist})
+
+def maxutest(p):
+    data = np.load(p,allow_pickle=True)
+    train = data[0]
+    test = data[1]
+    trainmerge = data[2]
+    unum = data[3]
+    pnum = data[4]
+    testmx = data2mx(test,unum,pnum)
+    trainmx = data2mx(train,unum,pnum)
+    tstart = time.time()
+    print('train mx is ',np.sum(trainmx))
+    print('test mx is ',np.sum(testmx))
+    algo = 1
+    f = 10
+    hrlist = np.arange(1,pnum,20)
+    #hrlist = np.arange(1,21)
+    hr = np.zeros([2, np.size(hrlist)])
+
+    # zero
+    pred = trainmodel(trainmx, testmx, f, algo)
+    hr[0, :] = calhrlist(pred, testmx, hrlist)
+
+    #full
+    a = trainmerge[9, :, :].reshape([train.shape[0], train.shape[1]])
+    trainmx = data2mx(a, unum, pnum)
+    pred = trainmodel(trainmx, testmx, f, algo)
+    hr[1, :] = calhrlist(pred, testmx, hrlist)
+    print(np.mean(trainmx[trainmx !=0]))
+    outp = 'test1130-itemcf.mat'
+    scio.savemat(outp, {'hr':hr,'hrlist':hrlist})
+    print('finish write')
+
+
+def fulltest(p):
+    data = np.load(p,allow_pickle=True)
+    train = data[0]
+    test = data[1]
+    trainmerge = data[2]
+    unum = data[3]
+    pnum = data[4]
+    testmx = data2mx(test,unum,pnum)
+    trainmx = data2mx(train,unum,pnum)
+    tstart = time.time()
+    print('train mx is ',np.sum(trainmx))
+    print('test mx is ',np.sum(testmx))
+    algo = 4
+    f = 10
+    hrlist = np.array([1,5,10,100,300,500,800,1000])
+    hr = np.zeros([11, np.size(hrlist)])
+
+    pred = trainmodel(trainmx, testmx, f, algo)
+    hr[0, :] = calhrlist(pred, testmx, hrlist)
+
+    print('%f seconds in i = 0' % (time.time() - tstart), hr[0,:])
+    for i in range(10):
+        a = trainmerge[i,:,:].reshape([train.shape[0], train.shape[1]])
+        trainmx = data2mx(a, unum, pnum)
+        pred = trainmodel(trainmx, testmx, f, algo)
+        hr[i+1, :] = calhrlist(pred, testmx, hrlist)
+        print('%f seconds in i = %d' % (time.time() - tstart,i+1), hr[i+1, :])
+    outp = 'test1130-3-warp.mat'
+    scio.savemat(outp, {'hr':hr,'hrlist':hrlist})
+    return hr
+
+
+def fulltest10time():
+    hr = np.zeros([10,11,8])
+    inp = 'data/yelppre_u200_p200_re.npy'
+    outp = 'tmp2/yelp-3'
+    for i in range(10):
+        trainsplit(inp, outp+'_'+str(i))
+        hr[i,:,:] = fulltest(outp+'_'+str(i)+'.npy')
+        print('finish big %d test'%i)
+    outp = 'test1130-4all-warp.mat'
+    scio.savemat(outp, {'hr': hr})
+    return hr
+
+
+def simutest1201(p):
+    # this is test for simu work
+    data = np.load(p,allow_pickle=True)
+    train = data[0]
+    test = data[1]
+    trainmerge = data[2]
+    unum = data[3]
+    pnum = data[4]
+    testmx = data2mx(test,unum,pnum)
+    alltrainmx = data2mx(train,unum,pnum) # explicit
+    a = trainmerge[9, :, :].reshape([train.shape[0], train.shape[1]])
+    trainmx = data2mx(a, unum, pnum)  # implicit
+    tstart = time.time()
+    print('train mx is ',np.sum(trainmx))
+    print('test mx is ',np.sum(testmx))
+    algo = 4
+    f = 10
+    hrlist = np.array([1,5,10,100,300,500,800,1000])
+
+    t = 300
+    iter = 10
+    hr = np.zeros([iter, np.size(hrlist)])
+    rate = np.zeros([iter] )
+    hitlist = np.zeros([unum])
+    for i in range(iter):
+        rate[i] = np.sum(hitlist) / unum
+        trainmx = gethitmx(trainmx, alltrainmx, hitlist)
+        pred = trainmodel(trainmx, testmx, f, algo)
+        hr[i, :] = calhrlist(pred, testmx, hrlist)
+        hitlist = calhit(pred,testmx,t,hitlist)
+        print('finish %d test'%i)
+        print(rate[i], hr[i,:])
+    outp = 'test1202-simu.mat'
+    scio.savemat(outp, {'hr': hr,'rate':rate,'t':t})
+
 
 
 
@@ -70,4 +198,7 @@ if __name__ == '__main__':
     inp = 'data/yelppre_u200_p200_re.npy'
     outp = 'data/yelptest-3.npy'
     #trainsplit(inp, outp)
-    mainmodel(outp)
+    #mainmodel(outp)
+    #maxutest(outp)
+    #fulltest10time()
+    simutest1201(outp)
